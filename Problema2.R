46615c482df027dd528d0a58ce9f683ceb68a27b
Janserranoxufre <jan.serrano@estudiantat.upc.edu>
2025-02-18 09:51:27 +0100

    Update Problema2.R
    18/2/25

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,33 @@
 #Codigo para problema 2
+
+mis_dades <- mtcars
+x <- mis_dades$cyl
+ni <- table(x)
+ni
+barplot(ni)
+#frquencia relativa (cuantos coches con x cilindros)
+fi <- ni/length(x)
+fi
+pie(fi)
+sum(fi)
+#frequencia acumulada (cuantos coche con hasra x cilindros)
+Ni <- cumsum(ni)
+Ni
+
+
+ni <- table(x)
+fi <- ni/length(x)
+Ni <- cumsum(ni)
+Fi <- cumsum(fi)
+cbind(ni, fi, Ni, Fi)
+
+mis_dades$mpg
+x <- cut(mis_dades$mpg, 10)
+ni <- table(x)
+fi <- ni/length(x)
+Ni <- cumsum(ni)
+Fi <- cumsum(fi)
+cbind(ni, fi, Ni, Fi)
+
+data <- mis_dades
+hist(data$mpg)
